8422becfb0686143e0f38962a9ba19722ade3f95
Marco <marco@gulinux.net>
2016-05-21 14:46:33 +0200

    Auto add properties

--- realcamera.cpp
+++ realcamera.cpp
@@ -25,6 +25,8 @@
 using namespace std;
 using namespace GuLinux;
 using namespace INDI::GPhoto;
+using namespace GPhotoCPP;
+using namespace INDI::Properties;
 class RealCamera::Private {
 public:
     enum ImageType { RAW, JPEG };
@@ -40,11 +42,11 @@ private:
     RealCamera *q;
 };
 
-RealCamera::Private::Private(INDI::CCD* device, RealCamera* q) 
-  : device{device},
-  log {device, "GPhotoCamera"},
-  image_parsers{{RAW, make_shared<GPhotoCPP::ReadRawImage>()}, {JPEG, make_shared<GPhotoCPP::ReadJPEGImage>()}},
-  q{q}
+RealCamera::Private::Private(INDI::CCD* device, RealCamera* q)
+    : device {device},
+log {device, "GPhotoCamera"},
+image_parsers {{RAW, make_shared<GPhotoCPP::ReadRawImage>()}, {JPEG, make_shared<GPhotoCPP::ReadJPEGImage>()}},
+q {q}
 {
     gphoto_logger = make_shared<GPhotoCPP::Logger>([&](const string &m, GPhotoCPP::Logger::Level l) {
         static map<GPhotoCPP::Logger::Level, INDI::Logger::VerbosityLevel> levels {
@@ -59,7 +61,7 @@ RealCamera::Private::Private(INDI::CCD* device, RealCamera* q)
     driver = make_shared<GPhotoCPP::Driver>(gphoto_logger);
     camera =  driver->autodetect();
     if(! camera)
-      throw std::runtime_error("Unable to find camera");
+        throw std::runtime_error("Unable to find camera");
 }
 
 
@@ -73,72 +75,116 @@ RealCamera::~RealCamera()
 
 vector< string > RealCamera::available_iso()
 {
-  return d->camera->settings().iso_choices();
+    return d->camera->settings().iso_choices();
 }
 
 string RealCamera::current_iso()
 {
-  return d->camera->settings().iso();
+    return d->camera->settings().iso();
 }
 
 bool RealCamera::set_iso(const string& iso)
 {
-  d->camera->settings().set_iso(iso);
-  d->camera->save_settings();
-  return current_iso() == iso;
+    d->camera->settings().set_iso(iso);
+    d->camera->save_settings();
+    return current_iso() == iso;
 }
 
 void RealCamera::shoot(INDI::GPhoto::Camera::Seconds seconds)
 {
-  d->current_shoot = d->camera->control().shoot(seconds);
+    d->current_shoot = d->camera->control().shoot(seconds);
 }
 
 INDI::GPhoto::Camera::ShootStatus RealCamera::shoot_status() const
 {
-  if(! d->current_shoot )
-    return {Camera::ShootStatus::Idle};
-  if( d->current_shoot->elapsed() >= d->current_shoot->duration() )
-    return {Camera::ShootStatus::Finished, d->current_shoot->elapsed(), Seconds{0} };
-  return {Camera::ShootStatus::Finished, d->current_shoot->elapsed(), d->current_shoot->duration() - d->current_shoot->elapsed() };
+    if(! d->current_shoot )
+        return {Camera::ShootStatus::Idle};
+    if( d->current_shoot->elapsed() >= d->current_shoot->duration() )
+        return {Camera::ShootStatus::Finished, d->current_shoot->elapsed(), Seconds{0} };
+    return {Camera::ShootStatus::Finished, d->current_shoot->elapsed(), d->current_shoot->duration() - d->current_shoot->elapsed() };
 }
 
 INDI::GPhoto::Camera::WriteImage RealCamera::write_image() const
 {
-  return [&](CCDChip &chip){
-    d->current_shoot->camera_file().wait();
-    d->log.session() << "Exposure complete, downloading image...";
-    GPhotoCPP::CameraFilePtr  file = d->current_shoot->camera_file().get();
-    d->current_shoot.reset();
-    string extension = make_stream(file->file().substr(file->file().rfind(".")+1)).transform<string>(::tolower);
-    auto image_parser = (extension == "jpg" || extension == "jpeg") ? d->image_parsers[Private::JPEG] : d->image_parsers[Private::RAW];
-    d->log.debug() << "Image filename" << file->file() << ", extension: " << extension;
-    vector<uint8_t> original_data = file->data();
-    GPhotoCPP::ReadImage::Image image;
-    try {
-      image = image_parser->read(original_data, file->file());
-    } catch(std::exception &e) {
-      d->log.error() << "Exposure failed to parse image: " << e.what();
-      return false;
-    }
-    d->log.debug() << "Copying image: w=" << image.w << ", h=" << image.h << ", bpp=" << image.bpp << ", channels=" << image.channels.size();
-    chip.setFrame(0, 0, image.w, image.h);
-    chip.setResolution(image.w, image.h);
-    chip.setNAxis(image.channels.size() == 3 ? 3 : 2);
-    chip.setBPP(image.bpp);
-    
-    typedef std::pair<GPhotoCPP::ReadImage::Image::Channel, GPhotoCPP::ReadImage::Image::Pixels> channel;
-    chip.setFrameBufferSize(make_stream(image.channels).transform<list<size_t>>([](const channel &c){ return c.second.size(); }).accumulate(), true);
-    size_t data_begin = 0;
-    for(auto c: image.channels) {
-      std::move(c.second.begin(), c.second.end(), chip.getFrameBuffer() + data_begin);
-      data_begin += c.second.size();
-    }
-    chip.setImageExtension("fits");
-    return true;
-  };
+    return [&](CCDChip &chip) {
+        d->current_shoot->camera_file().wait();
+        d->log.session() << "Exposure complete, downloading image...";
+        GPhotoCPP::CameraFilePtr  file = d->current_shoot->camera_file().get();
+        d->current_shoot.reset();
+        string extension = make_stream(file->file().substr(file->file().rfind(".")+1)).transform<string>(::tolower);
+        auto image_parser = (extension == "jpg" || extension == "jpeg") ? d->image_parsers[Private::JPEG] : d->image_parsers[Private::RAW];
+        d->log.debug() << "Image filename" << file->file() << ", extension: " << extension;
+        vector<uint8_t> original_data = file->data();
+        GPhotoCPP::ReadImage::Image image;
+        try {
+            image = image_parser->read(original_data, file->file());
+        } catch(std::exception &e) {
+            d->log.error() << "Exposure failed to parse image: " << e.what();
+            return false;
+        }
+        d->log.debug() << "Copying image: w=" << image.w << ", h=" << image.h << ", bpp=" << image.bpp << ", channels=" << image.channels.size();
+        chip.setFrame(0, 0, image.w, image.h);
+        chip.setResolution(image.w, image.h);
+        chip.setNAxis(image.channels.size() == 3 ? 3 : 2);
+        chip.setBPP(image.bpp);
+
+        typedef std::pair<GPhotoCPP::ReadImage::Image::Channel, GPhotoCPP::ReadImage::Image::Pixels> channel;
+        chip.setFrameBufferSize(make_stream(image.channels).transform<list<size_t>>([](const channel &c) {
+            return c.second.size();
+        }).accumulate(), true);
+        size_t data_begin = 0;
+        for(auto c: image.channels) {
+            std::move(c.second.begin(), c.second.end(), chip.getFrameBuffer() + data_begin);
+            data_begin += c.second.size();
+        }
+        chip.setImageExtension("fits");
+        return true;
+    };
 }
 
 
-void RealCamera::setup_properties(INDI::Properties::Properties< std::string >& properties)
+void RealCamera::setup_properties(::Properties< std::string >& properties)
 {
+    typedef GPhotoCPP::Widget Widget;
+    typedef const GPhotoCPP::WidgetPtr& WidgetPtr;
+    auto make_identity = [&](WidgetPtr w) {
+        return Identity {d->device->getDeviceName(), w->name(), w->label(), w->parent()->label(), w->access() == GPhotoCPP::Widget::ReadOnly ? IP_RO : IP_RW};
+    };
+    unordered_map<GPhotoCPP::Widget::Type, function<void(WidgetPtr)>> supported_types {
+        {   Widget::String, [&](WidgetPtr w) {
+                properties.add_text(w->name(), d->device, make_identity(w), [&](const vector<Text::UpdateArgs> &u) {
+                    string value = get<0>(u[0]);
+                    w->get<Widget::StringValue>()->set(value);
+                    d->camera->save_settings();
+                    return d->camera->widgets_settings()->child_by_name(w->name())->get<Widget::StringValue>()->get() == value;;
+                })
+                .add(w->name(), w->label(), w->get<Widget::StringValue>()->get().c_str());
+            }
+        },
+        {   Widget::Range, [&](WidgetPtr w) {
+		auto wv = w->get<Widget::RangeValue>();
+		properties.add_number(w->name(), d->device, make_identity(w), [&](const vector<Number::UpdateArgs> &u) {
+		  auto value= get<0>(u[0]);
+		  w->get<Widget::RangeValue>()->set(value);
+		  d->camera->save_settings();
+		  return w->get<Widget::RangeValue>()->get() == value;
+		})
+		.add(w->name(), w->label(), wv->range().min, wv->range().max, wv->range().increment, wv->get());
+            }
+        },
+        { Widget::Toggle, {} },
+        { Widget::Button, {} },
+        { Widget::Date, {} },
+        { Widget::Window, {} },
+        { Widget::Section, {} },
+        { Widget::Menu, {} },
+    };
+    auto widgets = make_stream(d->camera->widgets_settings()->all_children())
+    .filter([&](WidgetPtr w) {
+        return supported_types[w->type()];
+    })
+    .for_each([&](WidgetPtr w) {
+        supported_types[w->type()](w);
+    })
+                   ;
 }
